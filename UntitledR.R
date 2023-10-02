ed26ddccb01680a9e5caf921080da3994173c5cb
keniajin <keniajin@gmail.com>
2023-10-02 23:38:22 +0200

    Initial commit

---
+++ UntitledR.R
@@ -0,0 +1,11 @@
+# 
+# ## Terminologies and workflow    
+# - Git terminologies
+# - Repository, branch, commit, push, pull, fetch..
+# - Local and remote repositories
+# - Fork, clone, add, staged, 
+# - First Steps with Git
+# - The Basic Git Workflow
+library(here)
+create_project(here("~","applied_epi_test"))
+use_git(message = "Initial commit")
